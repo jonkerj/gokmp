a265a8447570222afa90573b706d655b7fc38f1a
Jorik Jonker <jorik.jonker@eu.equinix.com>
2023-10-02 20:55:38 +0200

    feat: add small test command

---
+++ cmd/test.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jonkerj/gokmp/pkg/client"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.bug.st/serial"
+)
+
+var (
+	testCmd = &cobra.Command{
+		Use:   "test",
+		Short: "Test heat meter",
+		Run:   testMeter,
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(testCmd)
+}
+
+func testMeter(cmd *cobra.Command, args []string) {
+	port, err := serial.Open(
+		viper.GetString("port"),
+		&serial.Mode{
+			BaudRate: 1200,
+			DataBits: 8,
+			StopBits: 2,
+			Parity:   serial.NoParity,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	err = port.SetReadTimeout(1600 * time.Millisecond)
+	if err != nil {
+		panic(err)
+	}
+
+	c := client.NewSerialClient(port)
+
+	sn, err := c.GetSerialNo()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("serial: %d\n", sn)
+}

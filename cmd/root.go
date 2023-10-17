f5129948669fd9db4947e71cab43ad0dfd4c3820
Jorik Jonker <jorik@kippendief.biz>
2023-10-17 14:36:15 +0200

    feat: add small test command

---
+++ cmd/root.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "gokmp",
+		Short: "tool to talk with your KMP meters",
+		Run:   root,
+	}
+)
+
+func init() {
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("gokmp")
+	flags := rootCmd.PersistentFlags()
+	flags.String("port", "/dev/ttyUSB0", "serial port")
+	viper.BindPFlags(flags)
+}
+
+func root(cmd *cobra.Command, args []string) {
+	fmt.Println("the root command does nothing, use the subcommands")
+}
+
+func Execute() {
+	rootCmd.Execute()
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/jonkerj/gokmp/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
